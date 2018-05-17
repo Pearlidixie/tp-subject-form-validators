772bcf783a181cd1eb4c54799d756d4c56b43b9f
pmalapeng <pmalapeng@gmail.com>
2018-05-17 14:45:09 +0200

    modified apps.py

--- tp_subject_form_validators/apps.py
+++ tp_subject_form_validators/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'tp_subject_form_validators'
-    verbose_name = 'TP Subject Screening'
+    verbose_name = 'TP Subject Form Validators'
